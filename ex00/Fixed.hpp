c7257281bc4131ba4be11143ba2f6e304898e9a9
subinprk <subpark@c4b12c3.42berlin.de>
2024-03-13 20:47:03 +0100

    ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,17 +6,27 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:07:26 by siun              #+#    #+#             */
-/*   Updated: 2024/03/13 18:49:55 by subpark          ###   ########.fr       */
+/*   Updated: 2024/03/13 20:42:22 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cmath>
+#include <iostream>
 
 class Fixed
 {
 	private:
-		
+		int					_fix_point;
+		static const int	_fract_bit = 8;
 	public:
-		Fixed(const int para);
-		Fixed
+		Fixed();
+		Fixed(const Fixed &copy);
+		Fixed & operator = (const Fixed &copy);
+		~Fixed();
+		int	getRawBits(void) const;
+		//const means that when this function is called
+		//on a const object of the class,
+		//it promises not to modify the object's data members
+		void	setRawBits(int const raw);
+
 };
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siun <siun@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:07:02 by siun              #+#    #+#             */
-/*   Updated: 2024/03/13 18:08:41 by siun             ###   ########.fr       */
+/*   Updated: 2024/03/13 20:39:08 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Fixed.hpp"
+#include "Fixed.hpp"
 
-int main()
+int main(void)
 {
-	return ;
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
